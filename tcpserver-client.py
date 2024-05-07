29bd8d538cc0bc62b91338edcebfe69785d07238
Ryan Cather <ryan.cather@ed.act.edu.au>
2024-05-07 22:50:20 +1000

    init

---
+++ tcpserver-client.py
@@ -0,0 +1,19 @@
+import socket
+
+def send_command_to_server(command):
+    host = "localhost"  # Change this to the server's hostname or IP address
+    port = 5000  # Use the same port number as the server
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))  # Connect to the server
+
+    client_socket.send(command.encode())  # Send the command to the server
+    response = client_socket.recv(1024).decode()  # Receive the response
+
+    print(f"Server response: {response}")
+
+    client_socket.close()  # Close the connection
+
+if __name__ == "__main__":
+    user_command = input("Enter a command to send to the server: ")
+    send_command_to_server(user_command)
